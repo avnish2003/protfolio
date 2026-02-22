83a95e0f0de67a964c85719c36099e62269363cb
Avnish <shriavnish333@gmail.com>
2026-02-22 22:10:56 +0530

    Initial React project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import Demo from './components/demo'
+
+
+const App=()=>{
+    return(
+        <div>
+            <Demo/>
+        </div>
+    )
+}
+export default App;
---
+++ src/components/Demo.jsx
@@ -0,0 +1,10 @@
+import react from "react";
+
+const Demo=()=>{
+    return(
+        <div>
+            <h1>heelo</h1>
+        </div>
+    )
+}
+export default Demo;
